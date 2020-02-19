8ca24c5bd3bcd371a7027bfb9fd9f0c97b4ea658
Jyhad AAMRI <happyman@Jyhads-MacBook-Pro.local>
2020-02-19 14:37:05 -0500

    Comment added to app.routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
     loadChildren: () => import('./modules/layout/layout.module').then(m => m.LayoutModule)
   },
   { path: 'MTM', redirectTo: 'recipes', pathMatch: 'prefix'},//Redirect all MTM requests to recipes
-  { path: 'mtm', redirectTo: 'recipes', pathMatch: 'prefix'},//Redirect all MTM requests to recipes
+  { path: 'mtm', redirectTo: 'recipes', pathMatch: 'prefix'},//Redirect all mtm requests to recipes => server requires this - case sensitive
   { path: '**', redirectTo: '', pathMatch: 'full' },
 
 ];
