cad9b64566409353f39bd06106267ee3208c388a
devynenterprise <jayde@stutor.ca>
2020-10-29 13:52:49 -0400

    more transaltions

--- src/app/modules/footer/footer.items.ts
+++ src/app/modules/footer/footer.items.ts
@@ -1,22 +1,22 @@
 
 export const SocialFooterItems = [
     {
-        name: 'Facebook',
+        name: 'facebook',
         href: 'https://www.facebook.com/MealsThatMatterCanada/',
         img: '/assets/static_images/footer/fb-logo.svg'
     },
     {
-        name: 'Instagram',
+        name: 'instagram',
         href: 'https://www.instagram.com/mealsthatmatter/',
         img: '/assets/static_images/footer/instagram.png'
     },
     {
-        name: 'Pinterest',
+        name: 'pinterest',
         href: 'https://www.pinterest.ca/MealsThatMatterCA/',
         img: '/assets/static_images/footer/pinterest.svg'
     },
     {
-        name: 'Amazon Alexa',
+        name: 'amazon alexa',
         href: 'https://www.amazon.ca/Unilever-Canada-Meals-that-MatterTM/dp/B07L2B32LQ/ref=pd_lutyp_crtyp_simh_1_1/147-4269172-2761155?_encoding=UTF8&pd_rd_i=B07L2B32LQ&pd_rd_r=a1999550-45b9-4e0a-a10f-8c3a8645e9c9&pd_rd_w=qkIEK&pd_rd_wg=IQyFU&pf_rd_p=d219e352-c0b8-4f3c-978b-56743d208e51&pf_rd_r=JXTW3Y2XS877YPZQ09YF&psc=1&refRID=JXTW3Y2XS877YPZQ09YF',
         img: '/assets/icons/MTM-favicon-72x72.png'
     },
@@ -24,17 +24,17 @@ export const SocialFooterItems = [
 ];
 
 export const AdFooterItems: IFooterItems[] = [{
-    name: 'Unilever',
+    name: 'unilever',
     href: 'https://www.unilever.ca',
     img: '/assets/static_images/footer/unilever-logo.svg'
 }];
 
 export const LegalFooterItems: IFooterItems[] = [{
-    name: 'Terms of use',
+    name: 'terms of use',
     href: 'https://www.unilever.ca/legal.html',
 
 }, {
-    name: 'Privacy Policy',
+    name: 'privacy policy',
     href: 'https://www.unilevernotices.com/canada/english/privacy-notice/notice.html'
 },
 {
@@ -45,11 +45,11 @@ export const LegalFooterItems: IFooterItems[] = [{
 ]
 
 export const BrandsFooterItems: IFooterItems[] = [{
-    name: 'Visit Knorr',
+    name: 'visit knorr',
     href: 'https://www.knorr.com/ca/en/home.html'
 },
 {
-    name: "Visit Hellmann's",
+    name: "visit hellmann's",
     href: 'https://www.hellmanns.com/ca/en/home.html'
 },
 
--- src/app/modules/home/home-carousel-helper.ts
+++ src/app/modules/home/home-carousel-helper.ts
@@ -1,25 +1,30 @@
 export const CarouselData = [{
-    header: 'CHEF CURATED RECIPES',
-    body: 'Explore a world of flavour with recipes hand-crafted and tested by our trusted global chefs.',
+    header: 'chef curated recipes',
+    description: 'Explore a world of flavour with recipes hand-crafted and tested by our trusted global chefs.',
+    body: 'chef curated recipes description',
     img: `/assets/static_images/Product_Benefits/Chef-curated-recipe.jpg`
 },
 {
-    header: 'INSTANT GROCERY LISTS',
-    body: 'Generate instant grocery lists from your meal plan and add or remove items as you wish.',
+    header: 'instant grocery list',
+    description: 'Generate instant grocery lists from your meal plan and add or remove items as you wish.',
+    body: 'instant grocery list description',
     img: `/assets/static_images/Product_Benefits/Instant-grocery-lists.jpg`
 },
 {
-    header: 'SHOPPING MADE EASY',
-    body: 'Find products easily online and avoid lines by getting groceries delivered or ready for pick up.',
+    header: 'shopping made easy',
+    description: 'Find products easily online and avoid lines by getting groceries delivered or ready for pick up.',
+    body: 'shopping made easy description',
     img: `/assets/static_images/Product_Benefits/Shopping-made-easy.jpg`
 },
 {
-    header: 'LESS TIME PREPPING',
-    body: 'Easy-to-create meal plans and instant grocery lists will give you more time back in your day.',
+    header: 'less time prepping',
+    description: 'Easy-to-create meal plans and instant grocery lists will give you more time back in your day.',
+    body: 'less time prepping description',
     img: `/assets/static_images/Product_Benefits/Less-time-prepping.jpg`
 },
 {
-    header: 'SAVE ON COSTS',
-    body: 'Save by cooking at home and using just the right amount of groceries for the recipes you love.',
+    header: 'save on costs',
+    description: 'Save by cooking at home and using just the right amount of groceries for the recipes you love.',
+    body: 'save on costs description',
     img: `/assets/static_images/Product_Benefits/Save-on-costs.jpg`
 }];
