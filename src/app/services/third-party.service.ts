967e82734cba52f09b7cc37becb0fd27902d1bcf
devynenterprise <jayde@stutor.ca>
2020-12-18 11:59:46 -0500

    www

--- src/app/services/third-party.service.ts
+++ src/app/services/third-party.service.ts
@@ -14,6 +14,6 @@ export class ThirdPartyService {
       // const clientSecret = environment.dropSecret;
       const clientSecret = '2557749db3be79907ec7ec7a139f8b288eb948628955ae0942a59b61dfd8a97f';
       // const sharedId = "a7c777b0-ce96-4554-9e01-3dc771bc0ab5"
-      await this.httpClient.get(`https://joindrop.com/partners/completions?partner_id=61&secret=${clientSecret}&shared_id=${sharedId}&status=APPROVED`).toPromise();
+      await this.httpClient.get(`https://www.joindrop.com/partners/completions?partner_id=61&secret=${clientSecret}&shared_id=${sharedId}&status=APPROVED`).toPromise();
   }
 }
