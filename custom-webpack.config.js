5f0b3edea5823494d9a353eaa4aa39e7ad016955
Arnaud Ober <arnaudober@oliver.agency>
2021-03-18 14:33:14 -0400

    Compression removed before pipeline

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,15 +1,15 @@
-const CompressionPlugin = require(`compression-webpack-plugin`);
-var BrotliPlugin = require('brotli-webpack-plugin');
+/*const CompressionPlugin = require(`compression-webpack-plugin`);
+var BrotliPlugin = require('brotli-webpack-plugin');*/
 var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 module.exports = {
     plugins:[
-        new BrotliPlugin({
+        /*new BrotliPlugin({
             asset: '[fileWithoutExt].[ext].br',
             test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/
         }),
         new CompressionPlugin({
             test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/
-        }),
+        }),*/
         new OptimizeCssAssetsPlugin({
             assetNameRegExp: /\.optimize\.css$/g,
             cssProcessor: require('cssnano'),
